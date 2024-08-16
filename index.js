f30750da833570d3dcfb6d6d28cbc3b08629d9c5
Đào Thị Kim Ngân <2351050111ngan@ou.edu.vn>
2024-08-17 02:09:55 +0700

    up

--- index.js
+++ index.js
@@ -1,11 +1,23 @@
 window.onload = function () {
-    document.querySelector('#more').addEventListener('click', function(event) {
+    document.querySelector('#more1').addEventListener('click', function(event) {
         event.preventDefault();
-        var moreDiv = document.querySelector('.more');
+        var moreDiv = document.querySelector('.more1');
         if (moreDiv.style.display === 'none') {
             moreDiv.style.display = 'block';
+            moreLink.textContent = 'Rút gọn ...';
         } else {
             moreDiv.style.display = 'none';
         }
     });
+    document.querySelector('#more2').addEventListener('click', function(event) {
+        event.preventDefault();
+        var moreDiv = document.querySelector('.more2');
+        if (moreDiv.style.display === 'none') {
+            moreDiv.style.display = 'block';
+            moreLink.textContent = 'Rút gọn ...';
+        } else {
+            moreDiv.style.display = 'none';
+        }
+    });
+    
 }
