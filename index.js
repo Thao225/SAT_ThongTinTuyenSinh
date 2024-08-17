65c9b1c300cfa263f5681db9cb0e729119d521ac
Đào Thị Kim Ngân <2351050111ngan@ou.edu.vn>
2024-08-17 11:36:41 +0700

    up

--- index.js
+++ index.js
@@ -1,23 +1,23 @@
-window.onload = function () {
-    document.querySelector('#more1').addEventListener('click', function(event) {
-        event.preventDefault();
-        var moreDiv = document.querySelector('.more1');
-        if (moreDiv.style.display === 'none') {
-            moreDiv.style.display = 'block';
-            moreLink.textContent = 'Rút gọn ...';
-        } else {
-            moreDiv.style.display = 'none';
-        }
-    });
-    document.querySelector('#more2').addEventListener('click', function(event) {
-        event.preventDefault();
-        var moreDiv = document.querySelector('.more2');
-        if (moreDiv.style.display === 'none') {
-            moreDiv.style.display = 'block';
-            moreLink.textContent = 'Rút gọn ...';
-        } else {
-            moreDiv.style.display = 'none';
-        }
-    });
+// window.onload = function () {
+//     document.querySelector('#more1').addEventListener('click', function(event) {
+//         event.preventDefault();
+//         var moreDiv = document.querySelector('.more1');
+//         if (moreDiv.style.display === 'none') {
+//             moreDiv.style.display = 'block';
+//             moreLink.textContent = 'Rút gọn ...';
+//         } else {
+//             moreDiv.style.display = 'none';
+//         }
+//     });
+//     document.querySelector('#more2').addEventListener('click', function(event) {
+//         event.preventDefault();
+//         var moreDiv = document.querySelector('.more2');
+//         if (moreDiv.style.display === 'none') {
+//             moreDiv.style.display = 'block';
+//             moreLink.textContent = 'Rút gọn ...';
+//         } else {
+//             moreDiv.style.display = 'none';
+//         }
+//     });
     
-}
+// }
