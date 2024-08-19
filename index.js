c5f8d1113dd621b92a1c7a46f12bf6220886f79f
Đào Thị Kim Ngân <2351050111ngan@ou.edu.vn>
2024-08-19 15:59:10 +0700

    up

--- index.js
+++ index.js
@@ -1,20 +1,4 @@
 window.onload = function () {
-// Lấy tất cả các phần tử có class 'menuXettuyen'
-var outB = document.querySelectorAll('.menuXettuyen');
-
-// Lặp qua từng phần tử và gán sự kiện 'onclick'
-outB.forEach(function(outerBlock) {
-    outerBlock.onclick = function() {
-        var inB = this.querySelectorAll('.displayHien');
-        
-        inB.forEach(function(innerBlock) {
-            // Kiểm tra nếu phần tử hiện tại có class 'hide'
-            if (innerBlock.classList.contains('hide')) {
-                innerBlock.classList.remove('hide'); // Hiển thị block trong
-            } else {
-                innerBlock.classList.add('hide'); // Ẩn block trong nếu đã hiển thị
-            }
-        });
-    };
-});
+    var thanhmenu = document.getElementsByClassName("thanhmenu");
+    
 }
