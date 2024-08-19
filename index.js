eecd48307b62cb7b456e4555afdbbeb0abbf687d
Đào Thị Kim Ngân <2351050111ngan@ou.edu.vn>
2024-08-19 22:36:09 +0700

    up

--- index.js
+++ index.js
@@ -1,4 +1,21 @@
 window.onload = function () {
-    var thanhmenu = document.getElementsByClassName("thanhmenu");
-    
+    let images = document.querySelectorAll(".anhGioiThieu img")
+    let popImg = document.getElementById("popImg")
+    let close = document.querySelector(".closeButton")
+    let pre = document.querySelector(".preButton")
+    let next = document.querySelector(".nextButton")
+    let gallery = document.querySelector(".gallery")
+        for (let img of images)
+            img.onclick = function () 
+            {
+                popImg.src = this.src;
+                gallery.classList.add("show");
+            }
+        close.onclick = function ()
+        { 
+            gallery.classList.remove("show");
+        } 
+
+        
+        pre.onclick = function () {
 }
