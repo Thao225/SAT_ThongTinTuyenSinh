72b26ce492ca1f217a5afd7e69399c49b5fc907e
kngan2201 <2351050111ngan@ou.edu.vn>
2024-08-31 08:06:10 +0700

    up

--- index.js
+++ index.js
@@ -23,7 +23,47 @@ window.onload = function () {
     closemenu.onclick = function () {
         thanhMenu.classList.remove("show");
     } 
-    
+//---------- ẢNH GIỚI THIỆU ---------------------------------------------------------------//
+    let listImg = document.querySelector(".listImg")
+    let imageGT = document.querySelectorAll(".gallery .image")
+    var current = 0;
+    var w = 800;
+    const changeImg = function() {
+        if (current == imageGT.length-1) {
+            listImg.style.transition = 'transform 0s';
+            listImg.style.transform = `translateX(${0}px)`;
+            current = 0; 
+        }
+        else {
+            current++;
+            listImg.style.transform = `translateX(${-w * current}px)`;
+        }
+    }
+    let clear = setInterval(() => { changeImg()}, 2500);
+    let prev = document.querySelector(".btnGT .prev")
+    let next = document.querySelector(".btnGT .next")
+    console.log(prev, next)
+        prev.onclick = function() {
+            clearInterval(clear);
+            if (current == 0) {
+                current = imageGT.length-1; 
+                listImg.style.transform = `translateX(${-w * current}px)`;
+            }
+            else {
+                current--;
+                listImg.style.transform = `translateX(${-w * current}px)`;
+            }
+            clear = setInterval(() => { changeImg()}, 2500); 
+        }
+        next.onclick = function() {
+            clearInterval(clear);
+            changeImg();
+            clear = setInterval(() => { changeImg()}, 2500);
+        }
+//---------- ẢNH GIỚI THIỆU ---------------------------------------------------------------//
+
+
+
 
 
 }
